ae98152d2f8dab5bdde5ec41fe9550f7170d3aa1
Markus Olsen <molsen13@gmail.com>
2015-06-11 10:29:02 -0600

    Sending messages back to the server with button click

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+io.on('connection', function (socket) {
+  console.log('Someone has connected 👀');
+
+  var counter = 0;
+
+  var interval = setInterval(function () {
+    socket.emit('message', {
+     user: 'turingbot',
+     message: 'Message #' + counter++
+    });
+  }, 1000);
+
+  socket.emit('message', { user: 'turingbot', text: 'yolo' });
+
+  socket.on('message', function (channel, message) {
+    console.log('MESSAGE FROM THE CLIENT -->', message);
+  });
+
+  socket.on('disconnect', function () {
+    clearInterval(interval);
+  });
+});
+
 http.listen(process.env.PORT || 3000, function(){
   console.log('Your server is up and running on Port 3000. Good job!');
 });
---
+++ public/application.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('I AM CONNECTED VIA A WEBSOCKET!!');
+});
+
+socket.on('message', function (message) {
+  //$('.messages').append(JSON.stringify(message) + '<br>');
+});
+
+var $messageInput = $('#special-message');
+
+$('#send-message').on('click', function () {
+  socket.send('message', {
+    username: 'markus',
+    text: $messageInput.val()
+  });
+});
